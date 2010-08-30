243dec5905ca12a33742e17f92c014296d22aa54
Jake <github@jake.otherinbox.com>
2010-08-29 18:06:21 -0700

    Expanded template and trimmed down binary.

--- templates/grape.rb
+++ templates/grape.rb
@@ -2,6 +2,9 @@ require 'grape_juice'
 
 # cat /usr/lib/ruby/user-gems/1.8/gems/rails-2.3.8/lib/rails_generator/generators/applications/app/template_runner.rb
 
+app_name = self.root.split('/').last
+
+
 #puts "In Template"
 #puts "Command Line"
 #puts $command_line.inspect
@@ -18,7 +21,20 @@ log/*.log
 tmp/**/*
 db/*.sqlite3
 versions
+coverage
+coverage.data
+db/*.sqlite3
+db/*.sqlite3-journal
+/config/database.yml
+/config/initializers/mail.rb
+/config/initializers/session_store.rb
+/config/fed_ex.yml
+/config/s3.yml
 END
+
+run "cp config/database.yml config/database.yml.example"
+run "cp config/initializers/mail.rb config/initializers/mail.rb.example"
+run "cp config/initializers/session_store.rb config/initializers/session_store.rb.example"
 
 
 #	For the 'Ruby Gem'
@@ -30,7 +46,7 @@ append_file 'Rakefile',<<-END
 begin
 	require 'jeweler'
 	Jeweler::Tasks.new do |gem|
-		gem.name = "#{$app_name}"
+		gem.name = "#{app_name}"
 		gem.summary = %Q{one-line summary of your gem}
 		gem.description = %Q{longer description of your gem}
 		gem.email = "insert_your_email@here.com"
@@ -70,12 +86,24 @@ file "uninstall.rb", <<-END
 END
 
 file 'rails/init.rb', <<-END
-require '#{$app_name}'
+require '#{app_name}'
 END
 
-file "lib/#{$app_name}.rb", <<-END
+file "lib/#{app_name}.rb", <<-END
+module #{app_name.camelize}
+#	predefine assumed namespace
+end
+# From http://guides.rubyonrails.org/plugins.html#controllers
+# Fix for:
+# ArgumentError in _some_Controller#_some_action_
+# A copy of ApplicationController has been removed from the module tree but is still active!
+# Equivalent of using "unloadable" in _some_Controller (unloadable has been deprecated)
+%w{models controllers}.each do |dir|
+  path = File.expand_path(File.join(File.dirname(__FILE__), '../app', dir))
+  ActiveSupport::Dependencies.load_once_paths.delete(path)
+end
 END
 
-inside "lib/#{$app_name}" do
+inside "lib/#{app_name}" do
 end
 
